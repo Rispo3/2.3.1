cdf8147a0ae38d96adb92040906701cb618d0534
Rispo3 <ltyutyunnikov74@gmail.com>
2023-01-17 21:09:07 +0300

    на проверку

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -8,6 +8,7 @@ import web.model.User;
 import web.service.UserService;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private UserService userService;
@@ -17,9 +18,36 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping("/users")
+    @GetMapping
     public String printUsers(Model model) {
         model.addAttribute("users", userService.allUsers());
-        return "users";
+        return "User/users";
+    }
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "User/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+        userService.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/update")
+    public String Edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("user", userService.get(id));
+        return "User/update";
+    }
+
+    @PatchMapping("/{id}")
+    public String Update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.update(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String Delete(@PathVariable("id") int id){
+        userService.delete(id);
+        return "redirect:/users";
     }
 }
--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,6 +1,6 @@
 package web.dao;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import web.model.User;
 
 import javax.persistence.EntityManager;
@@ -8,7 +8,7 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 
-@Repository
+@Component
 public class UserDaoImp implements UserDao {
 
     @PersistenceContext
